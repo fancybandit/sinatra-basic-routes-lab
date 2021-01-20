8b8d578adc56a368b46159ecc18f4beb1aebdc7d
Jacob Klimenko <droidedjake@gmail.com>
2021-01-20 01:01:37 -0500

    commit tree 48ea25609e15b0a6b62b2c990e9be27b24ce53f3

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Jake"
+    end
+
+    get '/hometown' do
+        "My hometown is Earth"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Orbital - Halcyon (Tom Middleton Re-Model'"
+    end
+
 end
+
